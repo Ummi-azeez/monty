32d33e50c76e4c237cb31141fc97de7d68369c84
Ummi <aishah.abdulazeez4@gmail.com>
2023-01-23 08:25:15 +0000

    stacks, queue and monty

---
+++ stackopcodes.c
@@ -0,0 +1,16 @@
+.lifi = 0;
+}
+
+/**
+ * f_queue - sets the flag
+ * @stack: stack head
+ * @line_number: line_number
+ * Return: void
+*/
+void f_queue(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+	bus.lifi = 1;
+}
+
